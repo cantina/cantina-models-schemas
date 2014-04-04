bef166734e0482fc5400a484851f7c8d8fcf3bd0
Dan MacTough <danmactough@gmail.com>
2014-04-04 14:20:56 -0400

    Add real tests

--- test/basic.js
+++ test/basic.js
@@ -1,6 +1,33 @@
 describe('basic', function () {
 
-  var app;
+  var nested = require('nested-objects');
+
+  var app
+    , schema = {
+        _name: 'test',
+        properties: {
+          id: {
+            type: 'string',
+            required: true,
+            validators: [(function (val) { return 'number' === typeof val; })]
+          },
+          name: {
+            first: {
+              type: 'string',
+              validators: [validateString]
+            },
+            last: {
+              type: 'string',
+              validators: [validateString],
+              default: ''
+            }
+          }
+        }
+      };
+
+  function validateString (val) {
+    return val.match(/^[A-Za-z -]+$/);
+  }
 
   before(function (done) {
     app = require('cantina');
@@ -21,5 +48,44 @@ describe('basic', function () {
     assert(app.schemas);
     assert.equal(typeof app.schemas.parse, 'function');
     assert.equal(typeof app.schemas.extend, 'function');
+  });
+
+  it('can parse a schema', function () {
+    var parsed = app.schemas.parse(schema);
+    assert(parsed);
+    assert(parsed.create);
+    assert(parsed.save);
+    assert.equal(parsed.create.length, 1);
+    assert.equal(parsed.save.length, 4);
+  });
+
+  it('can extend a schema', function () {
+    schema = app.schemas.extend(schema, {
+      properties: {
+        name: {
+          first: {
+            required: true
+          }
+        }
+      }
+    });
+    assert.strictEqual(nested.get(schema, 'properties.name.first.required'), true);
+    assert.strictEqual(nested.get(schema, 'properties.name.first.type'), 'string');
+    assert.strictEqual(nested.get(schema, 'properties.name.last.type'), 'string');
+    var parsed = app.schemas.parse(schema);
+    assert(parsed);
+    assert(parsed.create);
+    assert(parsed.save);
+    assert.equal(parsed.create.length, 1);
+    assert.equal(parsed.save.length, 5);
+  });
+
+  it('can generate options to pass into cantina-models', function () {
+    var options = app.schemas.getCollectionOptions(schema);
+    assert(options);
+    assert(options.create);
+    assert(options.save);
+    assert.equal(typeof options.create, 'function');
+    assert.equal(typeof options.save, 'function');
   });
 });
