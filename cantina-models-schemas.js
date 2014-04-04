d756d7d6ddc60aad404864715b94b6c8941a2cd5
Dan MacTough <danmactough@gmail.com>
2014-04-04 14:20:42 -0400

    Refactor #extend and #parse. Add #getCollectionOptions.

--- cantina-models-schemas.js
+++ cantina-models-schemas.js
@@ -1,14 +1,16 @@
 var app = require('cantina')
+  , clone = require('clone')
   , nested = require('nested-objects');
 
 app.schemas = {
+  getCollectionOptions: getCollectionOptions,
   parse: parseSchema,
   extend: extendSchema
 };
 
-function parseSchema (schema) {
+function getCollectionOptions (schema) {
   if (!schema._name) throw new Error('A schema requires a _name property');
-  var defs = findDefs(schema)
+  var defs = parseSchema(schema)
     , parsed = {};
 
   for (var def in defs) {
@@ -43,7 +45,7 @@ function parseSchema (schema) {
 
 }
 
-function findDefs (schema) {
+function parseSchema (schema) {
   var defs = {
         create: [],
         save: [],
@@ -112,5 +114,66 @@ function findDefs (schema) {
 }
 
 function extendSchema (destination, source) {
-  return app.defaults(true, destination, source);
+  return _defaults(true, {}, source, destination);
+}
+
+/**
+ * Extend an object with defaults. (Transplanted from Cantina 2).
+ *
+ * @param [deep] {Boolean} If true, performs a deep (recursive) merge.
+ * @param obj {Object} The object to extend.
+ * @param defaults {Object} An object containing the defaults.
+ */
+function _defaults (deep, obj, defaults) {
+  var copy;
+
+  // Shallow.
+  if (deep !== true) {
+    defaults = obj;
+    obj = deep;
+    deep = false;
+  }
+
+  // Support arbitrary number of defaults objects.
+  if (arguments.length > 2) {
+    var mixins = Array.prototype.slice.call(arguments, (deep === true) ? 2 : 1);
+    if (mixins.length > 1) {
+      mixins.forEach(function (mixin) {
+        if (deep === true) {
+          _defaults(true, obj, mixin);
+        }
+        else {
+          _defaults(obj, mixin);
+        }
+      });
+      return obj;
+    }
+  }
+
+  // Clone the defaults so we dont transfer any properties by reference.
+  if (deep === true) {
+    copy = clone(defaults);
+  }
+  else {
+    copy = defaults;
+  }
+
+  Object.keys(copy).forEach(function(key) {
+    // Only dive into object literals.
+    if (deep && _isObjectLiteral(obj[key]) && _isObjectLiteral(copy[key])) {
+      _defaults(deep, obj[key], copy[key]);
+    }
+    else if (!obj.hasOwnProperty(key)) {
+      obj[key] = copy[key];
+    }
+  });
+
+  return obj;
+}
+
+/**
+ * Check if an object is an object literal. (Transplanted from Cantina 2).
+ */
+function _isObjectLiteral (obj) {
+  return Object.prototype.toString.call(obj) === '[object Object]';
 }
